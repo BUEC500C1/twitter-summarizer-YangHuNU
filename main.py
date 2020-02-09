d6ed7ea7c150ffd4e8e63ad9fb551dd062f6da87
Yang Hu <s.guyuemuyi@gmail.com>
2020-02-09 16:00:59 -0500

    Test case

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import json
 from google_vision_api import google_vision
 from twitter_api import get_all_tweets
 
-def main():
-	print(get_all_tweets("Yang199703"))
+def main(user):
+	print(get_all_tweets(user))
 
 if __name__ == '__main__':
-	main()
+	main("Yang199703")
--- test_main.py
+++ test_main.py
@@ -1,7 +1,15 @@
 import pytest
+from main import main
+
+prog_pass = 1
 
 def test_sample1():
-
+	try: 
+		main("Yang199703")
+	except(KeyError, NameError):
+		assert prog_pass == 0
+	else:
+		assert prog_pass == 1
 
 def test_sample2():
 
